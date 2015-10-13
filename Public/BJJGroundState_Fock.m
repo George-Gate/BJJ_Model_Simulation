d95e9338d89c974b23d115e5318deda0633eda5f
George-Gate <terrorist_1992@hotmail.com>
2015-10-13 14:21:47 +0800

    [Public]改成直接调用makeState函数生成基态
    改成直接调用makeState函数生成基态;
    自动计算基态与SCS的Fedelity,并在Command Window中显示。

--- Public/BJJGroundState_Fock.m
+++ Public/BJJGroundState_Fock.m
@@ -22,62 +22,23 @@
 % N-1 bosons. Or always act annihilation before creation. 
 %
 %% parameters
-J=0;
+J=30;
 Ec=-1;
-N=20;     % maximum boson number in the space
-N0=18;    % boson number of the ground state
-clear minPsi minE;
+N=25;     % maximum boson number in the space
+N0=25;    % boson number of the ground state
 
 %% init operators
 generateFockOperators();
 H=sparse(-J*(a1'*a2+a2'*a1)+Ec/8*(a2'*a2-a1'*a1)^2);
 
 %% find ground state
-% find an initial state
-% only in N0 boson space
-Nindex=zeros(N0+1,1);
-for i=0:N0            % find the index of N-boson state
-    Nindex(i+1)=nn2k(i+1,N0-i+1);
-end
-% init minE and minPsi
-if (~exist('minE','var') || ~exist('minPsi','var'))
-    minPsi=zeros(Dim,1);
-    minPsi(Nindex)=randPsi(N0+1);
-    minE=real(minPsi'*H*minPsi);
-end
-% use randPsi to find an init state with low energy
-for i=1:100
-    psi=minPsi;
-    psi(Nindex)=psi(Nindex)+randPsi(N0+1);
-    psi=psi/sqrt(psi'*psi);
-    tmp=real(psi'*H*psi);
-    if (tmp<minE)
-        minPsi=psi;
-        minE=tmp;
-    end
-end
+minPsi=makeState('BJJ Ground','psi',nn2k,N0,Dim,H);
 
-% use imaginary time evolution method to get ground state
-dt=10/max(1,abs(minE));
-tol=1e-12;
-Udt=sparse(expm(-H*dt));
-while(1)
-    for i=10/dt:-1:1
-        psi_new=Udt*minPsi;    % evolve dt time
-        psi_new=psi_new/norm(psi_new);   % renormalize
-    end
-    if (1-psi_new'*minPsi < tol)     % check whether reached required precision
-        minPsi=psi_new;
-        % make output state pure real
-        minPsi=minPsi*exp(-1i*angle(max(minPsi)));
-        minE=real(minPsi'*H*minPsi); % update minE and exit
-        break;
-    else
-        minPsi=psi_new;      % update new state
-    end
-end
-% display ground state
-displayFockState(k2nn,minPsi,'|minPsi> = ');
+psiSCS=makeState('SCS','psi',nn2k,N0,Dim,J);
+display(['Fedelity with SCS=',num2str(abs(minPsi'*psiSCS),5)]);
+
 
 %% plot ground state distribution
 plotFockState(minPsi,N0,nn2k);
+% display ground state
+displayFockState(k2nn,minPsi,'|minPsi> = ');
